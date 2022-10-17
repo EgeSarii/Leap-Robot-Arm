797e4ef38692fc42b75a4edda12ab381e8a65942
Ege Sari <e.sari@student.science.ru.nl>
2022-10-17 16:33:52 +0200

    A new movement function is added to the arm.py. It will be helpful for our further work.

--- Robot_arm_code/arm.py
+++ Robot_arm_code/arm.py
@@ -31,6 +31,52 @@ def test_arm_1_and_2(arm1, arm2):
         arm2.set_angle(0)
         time.sleep(1)
 
-arm1 = Arm(0,16)
-arm2 = Arm(1,17)
-test_arm_1_and_2(arm1, arm2)
+
+def test_arm(arm):
+    '''
+    A tester function for arm. It tests the servo by changing the angle in all possible ways.
+    '''
+    while 1:
+        for i in range(-90, 90):
+            print("Angle is {}".format(i))
+            arm.set_angle(i)
+            time.sleep(0.1)
+            
+            
+def go_get_it():
+    '''
+    A grab and leave function for our further work. It grabs a piece of paper from a 
+    proper position. Then it takes the paper to a wanted position.
+    '''
+    arm1 = Arm(0,16)
+    arm2 = Arm(1, 17)
+    arm3 = Arm(2, 18)
+    arm4 = Arm(3, 19)
+    arm5 = Arm(4,20)
+    arm6 = Arm(5,21)
+    
+    
+    arm1.set_angle(-90)
+    time.sleep(1)
+    arm2.set_angle(60)
+    time.sleep(1)
+    arm3.set_angle(90)
+    time.sleep(1)
+    arm4.set_angle(50)
+    time.sleep(1)
+    arm5.set_angle(0)
+    time.sleep(1)
+    arm6.set_angle(-90)
+    time.sleep(2)
+    arm6.set_angle(0)
+    time.sleep(1)
+    arm2.set_angle(0)
+    time.sleep(1)
+    arm1.set_angle(30)
+    time.sleep(1)
+    arm6.set_angle(-90)
+    
+
+
+go_get_it()
+
