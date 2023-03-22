53f52c4827e825a497b69616a2cc02a2753771f8
Ege Sari <e.sari@student.science.ru.nl>
2023-03-22 12:03:23 +0100

    Inverse Kinematics function variable names changed

--- LepoBot/Leap Motion/Leap_motion_script/inverse_kinematics.cpp
+++ LepoBot/Leap Motion/Leap_motion_script/inverse_kinematics.cpp
@@ -13,7 +13,7 @@ double rad2deg(double angle_rad)
     return angle_rad * 180.0 / M_PI;
 }
 
-void ik_3(double x, double y,  double z, double& theta0, double& theta1, double& theta2, double& theta3)
+void ik(double x, double y,  double z, double& theta1, double& theta2, double& theta3, double& theta4)
 {
 
    
@@ -27,22 +27,22 @@ void ik_3(double x, double y,  double z, double& theta0, double& theta1, double&
     double py=y;
     double pz;
 
-    if( (x>= 19)  && x<=(a1+a2+a3)){
+    if( (x>= a1)  && x<=(a1+a2+a3)){
         px = x;
     }   
-    else if(x <19){
-        px =19;
+    else if(x <a1){
+        px =a1;
     }
     else
     { 	
         px =(a1+a2+a3);
     }   
     
-    if(z <= sqrt(729 -( (x-19)*(x-19) ))){
+    if(z <= sqrt(((a1+a2)* (a1+a2)) -((x-a1)*(x-a1)))){
         pz = z;
     }
     else{
-        pz = sqrt(729 -( (x-19)*(x-19) ));
+        pz = sqrt(((a1+a2)*(a1+a2)) -((x-a1)*(x-a1)));
     }
  	
   double phi = 10;
@@ -58,37 +58,37 @@ void ik_3(double x, double y,  double z, double& theta0, double& theta1, double&
   
     double s2_1 = sqrt(abs(1-c2*c2));
     double s2_2 = -sqrt(abs(1-c2*c2));
-    double theta_2_1 = atan2(s2_1, c2);
-    double theta_2_2 = atan2(s2_2, c2);
-    double denominator_1 = a1*a1 + a2*a2 + 2*a1*a2*cos(theta_2_1);
-    double denominator_2 = a1*a1 + a2*a2 + 2*a1*a2*cos(theta_2_2);
+    double theta_3_1 = atan2(s2_1, c2);
+    double theta_3_2 = atan2(s2_2, c2);
+    double denominator_1 = a1*a1 + a2*a2 + 2*a1*a2*cos(theta_3_1);
+    double denominator_2 = a1*a1 + a2*a2 + 2*a1*a2*cos(theta_3_2);
 
-    double s1_1 = (wy*(a1 + a2*cos(theta_2_1)) - (a2*sin(theta_2_1)*wx)) / denominator_1;
-    double s1_2 = (wy*(a1 + a2*cos(theta_2_2)) - (a2*sin(theta_2_2)*wx)) / denominator_2;
+    double s1_1 = (wy*(a1 + a2*cos(theta_3_1)) - (a2*sin(theta_3_1)*wx)) / denominator_1;
+    double s1_2 = (wy*(a1 + a2*cos(theta_3_2)) - (a2*sin(theta_3_2)*wx)) / denominator_2;
 
-    double c1_1 = (wx*(a1 + a2*cos(theta_2_1)) + (a2*sin(theta_2_1)*wy)) / denominator_1;
-    double c1_2 = (wx*(a1 + a2*cos(theta_2_2)) + (a2*sin(theta_2_2)*wy)) / denominator_2;
+    double c1_1 = (wx*(a1 + a2*cos(theta_3_1)) + (a2*sin(theta_3_1)*wy)) / denominator_1;
+    double c1_2 = (wx*(a1 + a2*cos(theta_3_2)) + (a2*sin(theta_3_2)*wy)) / denominator_2;
 
-    double theta_1_1 = atan2(s1_1, c1_1);
-    double theta_1_2 = atan2(s1_2, c1_2);
+    double theta_2_1 = atan2(s1_1, c1_1);
+    double theta_2_2 = atan2(s1_2, c1_2);
 
-    double theta_3_1 = phi - theta_1_1 - theta_2_1;
-    double theta_3_2 = phi - theta_1_2 - theta_2_2;
+    double theta_4_1 = phi - theta_2_1 - theta_3_1;
+    double theta_4_2 = phi - theta_2_2 - theta_3_2;
 
-    if (theta_1_1 >= theta_1_2)
+    if (theta_2_1 >= theta_2_2)
     {
-      theta1 = 0 + rad2deg(theta_1_1);
-      theta2 = 150 + rad2deg(theta_2_1) ;
-      theta3 = 60 -rad2deg(theta_3_1);
+      theta2 = 0 + rad2deg(theta_2_1);
+      theta3 = 150 + rad2deg(theta_4_1) ;
+      theta4 = 60 -rad2deg(theta_4_1);
     }
     else
     {
-      theta1 = 0 + rad2deg(theta_1_2);
-      theta2 = 150 + rad2deg(theta_2_2) ;
-      theta3 = 60 -rad2deg(theta_3_2);
+      theta2 = 0 + rad2deg(theta_2_2);
+      theta3 = 150 + rad2deg(theta_3_2) ;
+      theta4 = 60 -rad2deg(theta_4_2);
     }
 
-    theta0 = (py/50 * 180);
+    theta1 = (py/50 * 180);
   }
 }
 
